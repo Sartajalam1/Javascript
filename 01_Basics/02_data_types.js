ecae1ea1edbcb74dc13fd13c4692c35242d77506
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-11-10 17:42:58 +0530

    Create 02_data_types.js
    data types in details

---
+++ 01_Basics/02_data_types.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+console.log( 3 + 3);
+
+let name = "sartaj"
+let age = 19;
+let isLoggedIn = false;
+let state;
+
+
+
+// number => 2 to power 53  range
+// BigInt
+//string => ""
+// boolean => true/false
+// null => standaalone value
+// undefined =>
+//symbol => unique
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
