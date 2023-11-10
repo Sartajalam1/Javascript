6057a43c6f50053c5d44922b0405f876815a6aee
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-11-10 17:45:45 +0530

    Create 04_comparison.js
    comparision

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1);
+
+
+// fas ne fasane wala comparision , yaha par fas sakte hai
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) // true comparison convert null toa number, treating it as 0. thats why (3) null >= 0 is true and (1) null > 0is false.
+
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+
+// console.log("2" === 2)
+ 
