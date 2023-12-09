fd3f4c0134674e9af5714c86b4e5c508ce696405
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-12-09 17:44:33 +0530

    Create 02_scope.js

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,17 @@
+// Global scope
+// var c = 40; 
+
+// block scope
+let a = 400;
+if(true){
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+    console.log(a)
+}
+
+
+console.log(a)
+// console.log(b)
+// console.log(c) 
+
