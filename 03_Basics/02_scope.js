46f7c981b6a06d2abb00a50d3e9a20201bc61020
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-12-10 16:22:28 +0530

    Update 02_scope.js

--- 03_Basics/02_scope.js
+++ 03_Basics/02_scope.js
@@ -13,5 +13,44 @@ if(true){
 
 console.log(a)
 // console.log(b)
-// console.log(c) 
+// console.log(c)
+
+/************************ clouser & hoisting Next vedio ***************************/
+
+function one(){
+    const username = "sartaj"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+   
+    // two()
+}
+// one()
+
+if(true){
+    const username = "sagar"
+    if(username === "sagar"){
+        const website = "youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+}
+// console.log(username); 
+
+/******************************* interesting******************/
+console.log(addone(5))
+function addone(num){
+    return num + 1;
+}
+
+console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
+
 
