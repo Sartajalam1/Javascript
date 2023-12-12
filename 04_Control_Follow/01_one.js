7298278d5ab67b96d0f836d92725a1823231f966
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-12-13 00:45:16 +0530

    Create 01_one.js

---
+++ 04_Control_Follow/01_one.js
@@ -0,0 +1,83 @@
+ /*const isuserloggedin = true;
+ const temprature = 41;
+
+
+ if(temprature === 41){
+    console.log("Less than 50");
+ }
+ else{
+    console.log("temprature is greather than 50");
+ }
+ console.log("Executed")
+ 
+
+ // <,> ,<=, >=, ==, !=, ===, !==,  */
+
+/*  const score = 200;
+
+  if(score > 100){
+     const power = "fly"
+     console.log(`user Power: ${power}`)
+    }*/
+
+/*const balance = 1000
+
+if(balance > 50 ) console.log(`test`),console.log(`test2`);*/
+
+/* const balance = 1000;
+if(balance < 50){
+    console.log("Less than 500")
+}
+else if(balance < 750){
+    console.log(`Less than 750`)
+}
+else if(balance < 900){
+    console.log(`less than 900`)
+
+}
+else{
+    console.log(`less than 1200`)
+} */
+
+/*const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false;
+const loggedInFormEmail = true;
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log(`Allow to buy Course`)
+}
+if(loggedInFromGoogle || loggedInFormEmail){
+    console.log(`User Logged In`); 
+} */
+
+// Syntax Of Switch case
+/* switch(key){
+    case value:
+        break;
+
+        default:
+            break;
+} */
+
+const month =  "march"  //3;
+switch (month) {
+    case "jan":
+        console.log(`january`)
+        break;
+    case "feb":
+        console.log(`feb`)
+        break;
+    case "march":
+        console.log(`march`)
+        break;
+    case "April":
+        console.log(`April`)
+        break;
+
+    default:
+        console.log(`default case match`);
+        break;
+}
+
+ 
