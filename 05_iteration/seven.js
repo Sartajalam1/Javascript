7fa6e1ad2fb7eb69c43988f0391d3aa520a0c8dc
Sartaj alam <108234043+Sartajalam1@users.noreply.github.com>
2023-12-17 02:04:28 +0530

    Create seven.js

---
+++ 05_iteration/seven.js
@@ -0,0 +1,12 @@
+// Maps , This is Automatically return 
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNumbers.map((num)=>num*2)
+
+// This is a chaining method 
+const newNums = myNumbers
+.map((num) => num*10)
+.map((num) => num+1)
+.map((num) => num - 1)
+.filter((num) => num>=40)
+console.log(newNums);
